6b2ca10aa683703c0e375029578c2c13b4d84da5
Murilo Caixeta <murilocaixeta.udi@gmail.com>
2024-05-16 11:16:14 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert(document.cookie)
+// This script will execute the base64 code in the "code" parameter in the query string.
+
+const params = new URLSearchParams(window.location.search);
+eval(atob(params.get('code')));
